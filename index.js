c29fa4ee5f69a98c613267ee3842161a4f1af94e
John <shoemjoh@gmail.com>
2023-09-26 20:49:47 -0400

    control flow review lab

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue) {
   // Write your code here!
+  let result;
+  if (someValue <= 400) {
+    result = "This one is on me!"
+  }
+  else if (someValue > 400 && someValue <= 2000) {
+    result = "That will be twenty bucks."
+  }
+  else if (someValue > 2000 && someValue <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else {
+    result = "No can do."
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+  if (city == "NYC") {
+    result = "Ok, sounds good."
+  }
+  else {
+    result = "No go."
+  }
+  return result;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let result;
+
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.'
+      break;
+    case 'not as generous':
+      result = 'Thank you.'
+      break;
+    default:
+      result = 'Bye.';
+  }
+  return result;
+
 }
